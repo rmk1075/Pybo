9d2fffa485d99598c1ad36a46548e3cded4823ff
jamie <rmk1075@gmail.com>
2021-03-17 12:51:04 +0900

    feat: 페이징 기능 추가

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -16,7 +16,19 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 # question list 조회 함수
 @bp.route('/list/')
 def _list():
+    # 페이징 기능 추가
+    # GET 방식으로 페이지 값을 가져올때 사용한다.
+    # 페이지 값의 type은 int이다.
+    # default는 1, 첫번째 페이지이다.
+    page = request.args.get('page', type=int, default=1)
+
     question_list = Question.query.order_by(Question.create_date.desc())
+
+    # paginate 함수로 페이징을 적용한다.
+    # 첫번쨰 인자 page는 가져올 페이지의 번호이다,
+    # per_page는 페이지마다 보여줄 데이터의 건수이다.
+    question_list = question_list.paginate(page, per_page=10)
+
     return render_template('question/question_list.html', question_list=question_list)
 
 
